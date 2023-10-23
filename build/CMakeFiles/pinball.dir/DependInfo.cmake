7df7e166fe1d31ae3c595844d1179a16775ac826
GamaRL <gamarl1973@gmail.com>
2023-10-22 23:03:45 -0600

    ADD: Estructura del proyecto

---
+++ build/CMakeFiles/pinball.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/gamarl/Documentos/ingenieria/7 semestre/CGIHC/PinballMachine/src/helpers/Camera.cpp" "CMakeFiles/pinball.dir/src/helpers/Camera.cpp.o" "gcc" "CMakeFiles/pinball.dir/src/helpers/Camera.cpp.o.d"
+  "/home/gamarl/Documentos/ingenieria/7 semestre/CGIHC/PinballMachine/src/helpers/DirectionalLight.cpp" "CMakeFiles/pinball.dir/src/helpers/DirectionalLight.cpp.o" "gcc" "CMakeFiles/pinball.dir/src/helpers/DirectionalLight.cpp.o.d"
+  "/home/gamarl/Documentos/ingenieria/7 semestre/CGIHC/PinballMachine/src/helpers/Light.cpp" "CMakeFiles/pinball.dir/src/helpers/Light.cpp.o" "gcc" "CMakeFiles/pinball.dir/src/helpers/Light.cpp.o.d"
+  "/home/gamarl/Documentos/ingenieria/7 semestre/CGIHC/PinballMachine/src/helpers/Mesh.cpp" "CMakeFiles/pinball.dir/src/helpers/Mesh.cpp.o" "gcc" "CMakeFiles/pinball.dir/src/helpers/Mesh.cpp.o.d"
+  "/home/gamarl/Documentos/ingenieria/7 semestre/CGIHC/PinballMachine/src/helpers/Model.cpp" "CMakeFiles/pinball.dir/src/helpers/Model.cpp.o" "gcc" "CMakeFiles/pinball.dir/src/helpers/Model.cpp.o.d"
+  "/home/gamarl/Documentos/ingenieria/7 semestre/CGIHC/PinballMachine/src/helpers/PointLight.cpp" "CMakeFiles/pinball.dir/src/helpers/PointLight.cpp.o" "gcc" "CMakeFiles/pinball.dir/src/helpers/PointLight.cpp.o.d"
+  "/home/gamarl/Documentos/ingenieria/7 semestre/CGIHC/PinballMachine/src/helpers/Shader.cpp" "CMakeFiles/pinball.dir/src/helpers/Shader.cpp.o" "gcc" "CMakeFiles/pinball.dir/src/helpers/Shader.cpp.o.d"
+  "/home/gamarl/Documentos/ingenieria/7 semestre/CGIHC/PinballMachine/src/helpers/SpotLight.cpp" "CMakeFiles/pinball.dir/src/helpers/SpotLight.cpp.o" "gcc" "CMakeFiles/pinball.dir/src/helpers/SpotLight.cpp.o.d"
+  "/home/gamarl/Documentos/ingenieria/7 semestre/CGIHC/PinballMachine/src/helpers/Texture.cpp" "CMakeFiles/pinball.dir/src/helpers/Texture.cpp.o" "gcc" "CMakeFiles/pinball.dir/src/helpers/Texture.cpp.o.d"
+  "/home/gamarl/Documentos/ingenieria/7 semestre/CGIHC/PinballMachine/src/helpers/Window.cpp" "CMakeFiles/pinball.dir/src/helpers/Window.cpp.o" "gcc" "CMakeFiles/pinball.dir/src/helpers/Window.cpp.o.d"
+  "/home/gamarl/Documentos/ingenieria/7 semestre/CGIHC/PinballMachine/src/main.cpp" "CMakeFiles/pinball.dir/src/main.cpp.o" "gcc" "CMakeFiles/pinball.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/main.cpp
@@ -0,0 +1,221 @@
+/**
+ * Semestre 2024-1
+ * Proyecto Máquina de Pinball
+ */
+//para cargar imagen
+#include <glm/exponential.hpp>
+#include <glm/ext/matrix_transform.hpp>
+#include <glm/ext/vector_float3.hpp>
+#define STB_IMAGE_IMPLEMENTATION
+
+#include <stdio.h>
+#include <string.h>
+#include <cmath>
+#include <vector>
+#include <math.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+//para probar el importer
+//#include<assimp/Importer.hpp>
+
+#include "./include/Window.h"
+#include "./include/Mesh.h"
+#include "./include/Shader.h"
+#include "./include/Camera.h"
+#include "./include/Texture.h"
+#include "./include/Model.h"
+
+//para iluminación
+#include "./include/CommonValues.h"
+#include "./include/DirectionalLight.h"
+#include "./include/PointLight.h"
+#include "./include/SpotLight.h"
+
+
+Window mainWindow;
+std::vector<Mesh*> meshList;
+std::vector<Shader> shaderList;
+
+Camera camera;
+
+Texture brickTexture;
+Texture dirtTexture;
+Texture plainTexture;
+Texture pisoTexture;
+Texture llantaTexture;
+Texture fiTexture;
+Texture concreteTexture;
+Texture dado12Texture;
+
+Model Helicoptere;
+Model Helicoptere_Top;
+Model Helicoptere_Back;
+Model Helipad;
+
+//Sphere cabeza = Sphere(0.5, 20, 20);
+GLfloat deltaTime = 0.0f;
+GLfloat lastTime = 0.0f;
+static double limitFPS = 1.0 / 60.0;
+
+// luz direccional
+DirectionalLight mainLight;
+//para declarar varias luces de tipo pointlight
+PointLight pointLights[MAX_POINT_LIGHTS];
+SpotLight spotLights[MAX_SPOT_LIGHTS];
+
+// Vertex Shader
+static const char* vShader = "resources/shaders/shader_light.vert";
+// Fragment Shader
+static const char* fShader = "resources/shaders/shader_light.frag";
+
+void CreateObjects()
+{
+	unsigned int indices[] = {
+		0, 3, 1,
+		1, 3, 2,
+		2, 3, 0,
+		0, 1, 2
+	};
+
+	GLfloat vertices[] = {
+    //	x      y      z			u	  v			nx	  ny    nz
+			-1.0f, -1.0f, -0.6f,	0.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+			0.0f, -1.0f, 1.0f,		0.5f, 0.0f,		0.0f, 0.0f, 0.0f,
+			1.0f, -1.0f, -0.6f,		1.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+			0.0f, 1.0f, 0.0f,		0.5f, 1.0f,		0.0f, 0.0f, 0.0f
+	};
+
+	unsigned int floorIndices[] = {
+		0, 2, 1,
+		1, 2, 3
+	};
+
+	GLfloat floorVertices[] = {
+		-10.0f, 0.0f, -10.0f,	0.0f, 0.0f,		0.0f, -1.0f, 0.0f,
+		10.0f, 0.0f, -10.0f,	10.0f, 0.0f,	0.0f, -1.0f, 0.0f,
+		-10.0f, 0.0f, 10.0f,	0.0f, 10.0f,	0.0f, -1.0f, 0.0f,
+		10.0f, 0.0f, 10.0f,		10.0f, 10.0f,	0.0f, -1.0f, 0.0f
+	};
+
+	unsigned int vegetacionIndices[] = {
+	   0, 1, 2,
+	   0, 2, 3,
+	   4,5,6,
+	   4,6,7
+	};
+
+	GLfloat vegetacionVertices[] = {
+		-0.5f, -0.5f, 0.0f,		0.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+		0.5f, -0.5f, 0.0f,		1.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+		0.5f, 0.5f, 0.0f,		1.0f, 1.0f,		0.0f, 0.0f, 0.0f,
+		-0.5f, 0.5f, 0.0f,		0.0f, 1.0f,		0.0f, 0.0f, 0.0f,
+
+		0.0f, -0.5f, -0.5f,		0.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+		0.0f, -0.5f, 0.5f,		1.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+		0.0f, 0.5f, 0.5f,		1.0f, 1.0f,		0.0f, 0.0f, 0.0f,
+		0.0f, 0.5f, -0.5f,		0.0f, 1.0f,		0.0f, 0.0f, 0.0f,
+	};
+
+	Mesh *obj1 = new Mesh();
+	obj1->CreateMesh(vertices, indices, 32, 12);
+	meshList.push_back(obj1);
+
+	Mesh *obj2 = new Mesh();
+	obj2->CreateMesh(vertices, indices, 32, 12);
+	meshList.push_back(obj2);
+
+	Mesh *obj3 = new Mesh();
+	obj3->CreateMesh(floorVertices, floorIndices, 32, 6);
+	meshList.push_back(obj3);
+
+	Mesh* obj4 = new Mesh();
+	obj4->CreateMesh(vegetacionVertices, vegetacionIndices, 64, 12);
+	meshList.push_back(obj4);
+
+}
+
+void CreateShaders()
+{
+	Shader *shader1 = new Shader();
+	shader1->CreateFromFiles(vShader, fShader);
+	shaderList.push_back(*shader1);
+}
+
+
+
+int main()
+{
+	mainWindow = Window(1366, 768); // 1280, 1024 or 1024, 768
+	mainWindow.Initialise();
+
+	CreateObjects();
+	CreateShaders();
+
+	camera = Camera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), -60.0f, 0.0f, 0.3f, 0.5f);
+
+	unsigned int pointLightCount = 0;
+
+  unsigned int spotLightCount = 0;
+
+	GLuint uniformProjection = 0, uniformModel = 0, uniformView = 0, uniformEyePosition = 0,
+		uniformSpecularIntensity = 0, uniformShininess = 0;
+	GLuint uniformColor = 0;
+	glm::mat4 projection = glm::perspective(45.0f, (GLfloat)mainWindow.getBufferWidth() / mainWindow.getBufferHeight(), 0.1f, 1000.0f);
+
+  // Loop mientras no se cierra la ventana
+	while (!mainWindow.getShouldClose())
+	{
+		GLfloat now = glfwGetTime();
+		deltaTime = now - lastTime;
+		deltaTime += (now - lastTime) / limitFPS;
+		lastTime = now;
+
+
+    // Recibir eventos del usuario
+		glfwPollEvents();
+		camera.keyControl(mainWindow.getsKeys(), deltaTime);
+		camera.mouseControl(mainWindow.getXChange(), mainWindow.getYChange());
+
+    // Clear the window
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		shaderList[0].UseShader();
+		uniformModel = shaderList[0].GetModelLocation();
+		uniformProjection = shaderList[0].GetProjectionLocation();
+		uniformView = shaderList[0].GetViewLocation();
+		uniformEyePosition = shaderList[0].GetEyePositionLocation();
+		uniformColor = shaderList[0].getColorLocation();
+
+    // información en el shader de intensidad especular y brillo
+		uniformSpecularIntensity = shaderList[0].GetSpecularIntensityLocation();
+		uniformShininess = shaderList[0].GetShininessLocation();
+
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+		glUniformMatrix4fv(uniformView, 1, GL_FALSE, glm::value_ptr(camera.calculateViewMatrix()));
+		glUniform3f(uniformEyePosition, camera.getCameraPosition().x, camera.getCameraPosition().y, camera.getCameraPosition().z);
+
+    // luz ligada a la cámara de tipo flash
+    // sirve para que en tiempo de ejecución (dentro del while) se cambien 
+    // propiedades de la luz
+		glm::vec3 lowerLight = camera.getCameraPosition();
+		lowerLight.y -= 0.3f;
+
+    // información al shader de fuentes de iluminación
+		shaderList[0].SetDirectionalLight(&mainLight);
+
+    spotLights[0].SetFlash(lowerLight, camera.getCameraDirection());
+    shaderList[0].SetSpotLights(spotLights, spotLightCount);
+
+
+		glUseProgram(0);
+
+		mainWindow.swapBuffers();
+	}
+
+	return 0;
+}
