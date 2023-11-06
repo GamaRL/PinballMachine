9b624fce2f215820b5160c737ca44aaa37b8f721
GamaRL <gamarl1973@gmail.com>
2023-11-06 03:10:07 -0600

    ADD: Se texturiza la máquina de PinBall

--- src/main.cpp
+++ src/main.cpp
@@ -272,7 +272,7 @@ int main()
   Obstacle.LoadModel("resources/models/Obstacle_1.obj");
 
   Spring::Initialise();
-  Spring spring = Spring(43.0f, 0.0f, 80.0f);
+  Spring spring = Spring(43.5f, 3.0f, 80.0f);
 
   SpringMouseController springMouseController(&mainWindow, &spring);
 
@@ -390,7 +390,7 @@ int main()
 
     // Cubierta de la máquina de pinball
     model = glm::mat4(1.0f);
-    model = glm::translate(model, glm::vec3(0.0f, -91.0f, 0.0f));
+    model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
 		glUniform2fv(uniformTextureOffset, 1, glm::value_ptr(toffset));
